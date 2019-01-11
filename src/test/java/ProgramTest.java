6541ed5500838acf4a0f6b82c72f9fc52ee84e13
Hatikoed <mihail.edemskiy@gmail.com>
2019-01-12 01:58:45 +0300

    Переписан тест для логики открытия файла базы пользователей

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -1,15 +1,19 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class ProgramTest {
 
     @Test
-    public void open_file() {
+    public void open_file() throws IOException {
         Program program = new Program();
-        boolean actual = true;
-        boolean expected = program.open_file();
-        Assert.assertEquals(actual,expected);
+        String actual = "12345678";
+        Map<String,String> expected = program.open_file("C:\\Users\\mihai\\Desktop\\data_person.txt");
+        Assert.assertEquals(actual,expected.get("Test_User"));
     }
 }
