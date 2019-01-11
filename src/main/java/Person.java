59d7882972da6e4c9d0c882cc46534fd94899eb2
Hatikoed <mihail.edemskiy@gmail.com>
2019-01-11 23:40:21 +0300

    Создал методы

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,14 @@
 public class Person {
     public static void main(String[] args) {
-        
+
+    }
+    public boolean input(){
+        return false;
+    }
+    public boolean output(){
+        return false;
+    }
+    public boolean delete(){
+        return false;
     }
 }
