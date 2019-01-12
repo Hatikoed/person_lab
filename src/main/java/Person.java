c7f803902e43631238ec508d2c950295e90a84b9
Hatikoed <mihail.edemskiy@gmail.com>
2019-01-12 03:47:16 +0300

    Написан метод для добавления пользователя

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,6 @@
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
@@ -32,8 +35,29 @@ public class Person {
             return true;
         }
     }
-    public boolean register(){
-        return false;
+    public boolean register(Map<String, String> file, String user, String password, String path) throws NoSuchAlgorithmException, IOException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+
+        byte[] dataBytes = md.digest();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dataBytes.length; i++) {
+            sb.append(Integer.toString((dataBytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        String result = sb.toString();
+        String value = file.get(user);
+        if (value != null) {
+            System.out.println("Такой пользователь существует!");
+            return true;
+        } else {
+            String new_user = "\r\n" + user + " " + result;
+            FileOutputStream fos = new FileOutputStream(path,true);
+            byte[] buffer = new_user.getBytes();
+            fos.write(buffer, 0, buffer.length);
+            System.out.println("Пользователь добавлен!");
+            return true;
+        }
     }
     public boolean delete(){
         return false;
