8c5d3b06ef59d9f239a5f066d2b829073807d3c2
Hatikoed <mihail.edemskiy@gmail.com>
2019-01-12 04:09:14 +0300

    Добавлено взаимодействие с рабочими методами

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,11 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public class Program {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         Map<String, String> file = null;
+        String pathGlob = "";
         boolean b = true;
         while(b){
             System.out.println("Выберите один из вариантов:");
@@ -17,6 +19,9 @@ public class Program {
             System.out.println("5 - Выйти из программы");
             Scanner in = new Scanner(System.in);
             Scanner infile = new Scanner(System.in);
+            Scanner inuser = new Scanner(System.in);
+            Scanner innewuser = new Scanner(System.in);
+            Scanner innewuser2 = new Scanner(System.in);
             int value = in.nextInt();
             Person person = new Person();
             switch (value){
@@ -25,6 +30,7 @@ public class Program {
                     while (path.equals("")){
                         System.out.println("Введите путь к файлу:");
                         path = infile.nextLine();
+                        pathGlob = path;
                         file = open_file(path);
                         if (file == null){
                             path = "";
@@ -32,13 +38,30 @@ public class Program {
                     }
                     break;
                 case 2:
-                    person.input();
+                    System.out.println("Проверка пользователя...");
+                    System.out.println("Введите имя пользователя:");
+                    String user = inuser.nextLine();
+                    System.out.println("Введите пароль пользователя:");
+                    String password = inuser.nextLine();
+                    person.input(file, user, password);
                     break;
                 case 3:
-                    person.register();
+                    System.out.println("Добавление пользователя...");
+                    System.out.println("Введите имя пользователя:");
+                    String new_user = innewuser.nextLine();
+                    System.out.println("Введите пароль пользователя:");
+                    String new_password = innewuser.nextLine();
+                    person.register(file, new_user, new_password,pathGlob);
+                    file = open_file(pathGlob);
                     break;
                 case 4:
-                    person.delete();
+                    System.out.println("Удаление пользователя...");
+                    System.out.println("Введите имя пользователя:");
+                    String new_user2 = innewuser2.nextLine();
+                    System.out.println("Введите пароль пользователя:");
+                    String new_password2 = innewuser2.nextLine();
+                    person.register(file, new_user2, new_password2,pathGlob);
+                    file = open_file(pathGlob);
                     break;
                 case 5:
                     b = false;
