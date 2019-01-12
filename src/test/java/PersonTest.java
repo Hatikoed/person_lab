8d004e8b00a95a99d7fce8d85308f542928bc056
Hatikoed <mihail.edemskiy@gmail.com>
2019-01-12 03:08:44 +0300

    Переписан тест проверки пользователя

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,15 +1,21 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class PersonTest {
     @Test
-    public void input() {
+    public void input() throws IOException, NoSuchAlgorithmException {
         Person person = new Person();
-        boolean actual = true;
-        boolean expected = person.input();
-        Assert.assertEquals(actual,expected);
+        Program program = new Program();
+        Map<String,String> file = program.open_file("C:\\Users\\mihai\\Desktop\\data_person.txt");
+        String user = "Hatikoed";
+        String password = "1705";
+        Assert.assertEquals(true,person.input(file, user, password));
     }
 
     @Test
